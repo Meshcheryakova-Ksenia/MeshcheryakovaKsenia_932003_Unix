b0635bf2c30426b39d64e2e7161cb5dfad409818
Meshcheryakova-Ksenia <113160842+Meshcheryakova-Ksenia@users.noreply.github.com>
2023-11-17 17:10:31 +0700

    Add files via upload

---
+++ lab2/main.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Функция для генерации случайного идентификатора
+generate_id() {
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1
+}
+
+# Функция для получения следующего доступного имени файла
+get_next_filename() {
+exec 9>>/dev/null
+flock -x 9
+
+i=1
+while [ -f "/shared_volume/$(printf "%03d" $i)" ]; do
+i=$((i+1))
+done
+
+printf "%03d" $i
+
+flock -u 9
+}
+
+# Функция для создания и удаления файла с задержкой
+operate_file() {
+filename=$(get_next_filename)
+id=$(generate_id)
+
+echo "Контейнер $id создает файл $filename"
+echo "$id $filename" > "/shared_volume/$filename"
+
+sleep 1
+
+echo "Контейнер $id удаляет файл $filename"
+rm "/shared_volume/$filename"
+
+sleep 1
+}
+
+# Основной цикл программы
+while true; do
+operate_file
+done

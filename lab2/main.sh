91ba6f059baaeee449fb86c0643c6a6971f31369
Meshcheryakova-Ksenia <113160842+Meshcheryakova-Ksenia@users.noreply.github.com>
2023-11-25 15:47:33 +0700

    Update main.sh

--- lab2/main.sh
+++ lab2/main.sh
@@ -7,7 +7,7 @@ cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1
 
 # Функция для получения следующего доступного имени файла
 get_next_filename() {
-exec 9>>/dev/null
+exec 9</dev/null
 flock -x 9
 
 i=1
@@ -16,17 +16,19 @@ i=$((i+1))
 done
 
 printf "%03d" $i
-
-flock -u 9
 }
 
 # Функция для создания и удаления файла с задержкой
 operate_file() {
+
 filename=$(get_next_filename)
 id=$(generate_id)
 
 echo "Контейнер $id создает файл $filename"
 echo "$id $filename" > "/shared_volume/$filename"
+
+# снятие блокировки после создания файла
+exec 9<&-
 
 sleep 1
 
